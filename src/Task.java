390f8c3fef3ad257d8b155e1324d01b56a40d1af
Dmitry <dim4n94@yandex.ru>
2020-05-14 12:24:06 +0300

    Initial commit

---
+++ src/Task.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число в бинарном формате: ");
+        int num = in.nextInt();
+        String str = String.valueOf(num);
+        int n = 0;
+        for (int i = str.length() - 1, j = 1; i >= 0; i--, j *= 2) {
+            n += (str.charAt(i) - '0') * j;
+        }
+        System.out.println("Число в десятичном формате: " + n);
+    }
+}
