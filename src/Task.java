475311240f09b1bb7172891c5fae2d0d67d3e111
Dmitry <dim4n94@yandex.ru>
2020-05-15 21:11:43 +0300

    add new algorithm

--- src/Task.java
+++ src/Task.java
@@ -4,9 +4,8 @@ public class Task {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число в бинарном формате: ");
-        int num = in.nextInt();
-        String str = String.valueOf(num);
-        if (str.contains("2") || str.contains("3") || str.contains("4") || str.contains("5") || str.contains("6") || str.contains("7") || str.contains("8") || str.contains("9")) {
+        String str = in.next();
+        if (!str.matches("^[01]+$")) {
             System.out.println("Число имеет неверный формат!");
         } else {
             int n = 0;
