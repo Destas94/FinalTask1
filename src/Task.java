dae74f071ab68743079ca468f4b5e08c926e09f9
Dmitry <dim4n94@yandex.ru>
2020-05-15 11:54:06 +0300

    add check format

--- src/Task.java
+++ src/Task.java
@@ -6,10 +6,14 @@ public class Task {
         System.out.print("Введите число в бинарном формате: ");
         int num = in.nextInt();
         String str = String.valueOf(num);
-        int n = 0;
-        for (int i = str.length() - 1, j = 1; i >= 0; i--, j *= 2) {
-            n += (str.charAt(i) - '0') * j;
+        if (str.contains("2") || str.contains("3") || str.contains("4") || str.contains("5") || str.contains("6") || str.contains("7") || str.contains("8") || str.contains("9")) {
+            System.out.println("Число имеет неверный формат!");
+        } else {
+            int n = 0;
+            for (int i = str.length() - 1, j = 1; i >= 0; i--, j *= 2) {
+                n += (str.charAt(i) - '0') * j;
+            }
+            System.out.println("Число в десятичном формате: " + n);
         }
-        System.out.println("Число в десятичном формате: " + n);
     }
 }
